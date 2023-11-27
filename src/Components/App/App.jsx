f7352a80429d87b78801cb646a89b5e195fbcf99
Ian McIntosh <imcintosh22@turing.edu>
2023-11-27 20:13:05 +0000

    finished hero

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from '../Header/Header'
+import Hero from '../Hero/Hero'
 
 const App = () => {
   return (
     <>
     <Header />
+    <main>
+      <Hero />
+    </main>
     </>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import hero from '../../assets/hero.jpg'
+
+const Hero = () => {
+    return (
+        <section className='hero-container'>
+            <div className='hero-text-container'>
+                <h2>Vanta @ Haltom Theatre - November 11 2023</h2>
+                <p>Pictures from Vanta's second show performed at Haltom Theatre.  
+                    <br />Photos were taken by Ian McIntosh
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero

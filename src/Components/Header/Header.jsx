2645cde1fbe4fa8a0073972812a226cbec2aeb43
Ian McIntosh <imcintosh22@turing.edu>
2023-12-05 18:50:54 +0000

    changed the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Header.css'
+import vanta from '../../assets/vanta.jpg'
 
 const Header = () => {
     return (
         <header className='header-container'>
-            <h1 className='header-title'>Vanta</h1>
-            <p className='header-text'>@ Haltom Theater - <date>11/25/2023</date></p>
+            <div className='title-container'>
+                <h1 className='header-title'>Vanta</h1>
+                <p className='header-text'>@ Haltom Theater - <date>11/25/2023</date></p>
+            </div>
+
+            <img src={vanta} alt='band logo' className='vanta-logo'/>
+
+            <div className='link-container'>
+                <h2><a href=''>contact the band</a></h2>
+                <h2><a href='https://ianmac.live' target='_blank'>contact the photographer</a></h2>
+            </div>
         </header>
     )
 }
